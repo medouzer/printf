e93808aa8e874e97051e55dd37dbd56eea567835
mohamed ouzerzou <mohamed.ouz2016@gmail.com>
2023-04-26 18:21:03 -0700

    fixing error

--- print_number.c
+++ print_number.c
@@ -9,30 +9,23 @@
 
 int print_number(int num, int *k)
 {
-	if (num == -2147483648)
-	{
-		putchar('-');
-		putchar('2');
-		*k = *k + 2;
-		print_number(147483648, k);
-		return (0);
-	}
 	if (num < 0)
 	{
 		putchar('-');
-		*k += 1;
+		*k = *k + 1;
 		num *= (-1);
 	}
-	else if (num >= 10)
+
+	if (num >= 10)
 	{
 		print_number(num / 10, k);
 		putchar((num % 10) + '0');
 		*k += 1;
 	}
-	else
+	else if (num < 10)
 	{
 		putchar(num + '0');
-		*k += 1;
+		*k = *k + 1;
 	}
 
 	return (0);
